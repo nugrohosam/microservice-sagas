55dc9170674ebc9cd5cf982cd9d883ee000d879d
nugrohosam-dot <nugroho@dot-indonesia.com>
2022-01-04 06:42:38 +0700

    Fix Feature | Change copy validation

--- test-app/test/initial-test.js
+++ test-app/test/initial-test.js
@@ -3,12 +3,15 @@ const request = require('request');
 require('dotenv').config()
 
 describe('Initial test', function () {
-    it('test call service', function () {
-        assert.equal(true, true);
-        request.post({url: 'http://' + process.env.HOST_ORDER, formData: null}, function (error, response, body) {
-            console.error('error:', error); // Print the error if one occurred
-            console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-            console.log('body:', body); // Print the HTML for the Google homepage.
+    const timesTest = process.env.TIMES_TEST;
+    for (let i = 0; i < timesTest; i++){
+        it('test call service', function () {
+            assert.equal(true, true);
+            request.post({url: 'http://' + process.env.HOST_ORDER, formData: null}, function (error, response, body) {
+                console.error('error:', error); // Print the error if one occurred
+                console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+                console.log('body:', body); // Print the HTML for the Google homepage.
+            });
         });
-    });
+    }
 });
